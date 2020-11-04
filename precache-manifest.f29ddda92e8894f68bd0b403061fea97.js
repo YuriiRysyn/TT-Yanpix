3366a61fb8c2dd234cfadc147f2a765a4fe4f56f
Yurii Rysyn <zerocool_cs@ukr.net>
2020-11-04 18:52:25 +0200

    Updates

---
+++ precache-manifest.f29ddda92e8894f68bd0b403061fea97.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68614821826da635a6aea2924b64562",
+    "url": "/TT-Yanpix/index.html"
+  },
+  {
+    "revision": "a15918437db897adbd40",
+    "url": "/TT-Yanpix/static/css/main.4e15bfb8.chunk.css"
+  },
+  {
+    "revision": "fd05ad59f6c2591e19d5",
+    "url": "/TT-Yanpix/static/js/2.ccb67523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TT-Yanpix/static/js/2.ccb67523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15918437db897adbd40",
+    "url": "/TT-Yanpix/static/js/main.e104f50a.chunk.js"
+  },
+  {
+    "revision": "819a9fee96ef6b2b537a",
+    "url": "/TT-Yanpix/static/js/runtime-main.656df6d0.js"
+  },
+  {
+    "revision": "2c9eb36260b3cd2867647be779ca6477",
+    "url": "/TT-Yanpix/static/media/Grandstander-Medium.2c9eb362.ttf"
+  },
+  {
+    "revision": "0a64740c61b8aa13ea5e2dd0eb4726e1",
+    "url": "/TT-Yanpix/static/media/SharpSansNo2Bold.0a64740c.otf"
+  },
+  {
+    "revision": "20b773328c98c9f9c7ce59c551f05516",
+    "url": "/TT-Yanpix/static/media/SharpSansNo2Medium.20b77332.otf"
+  },
+  {
+    "revision": "c90f75b930746d6bc66e82baf982ca34",
+    "url": "/TT-Yanpix/static/media/SharpSansNo2Semibold.c90f75b9.otf"
+  }
+]);
